aa75fd251f42a879abe787b944faf9ecf930a6b7
Andre Franca <andreffranca@gmail.com>
2024-10-15 15:05:10 +0100

    hello world

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="ergodic",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=required,
+    author="ergodic.ai",
+    author_email="andre@ergodic.ai",
+    description="Python client for ergodic.ai",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ergodic-ai/ergodic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
