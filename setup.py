31fa696099b973248c5e06b68097b7fdd057df60
Andre Franca <andreffranca@gmail.com>
2024-10-22 18:42:46 +0100

    p

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="ergodic",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=required,
